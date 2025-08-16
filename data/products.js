f570c35c4e0731df34dbcfb81edaf755b7381d84
SunilG <sunilbohora1010@gmail.com>
2025-08-16 15:29:39 +0500

    Version2: making data

--- data/products.js
+++ data/products.js
@@ -1,7 +1,26 @@
 const products = [
   {
     id: 8888399612064,
-    name: "Hibis Box Tissue, ",
-    description: "soft white facila tissue",
+    name: "Hibis Box Tissue",
+    description: "soft white facial tissue",
+    image: "Images/hibis-tissue.png",
+    priceLaari: 1200,
+    category: "households",
+  },
+  {
+    id: 8690216360108,
+    name: "Fatih Dust-Free Erasers",
+    description: "Phthalate free, Latex-free",
+    image: "Images/fatih-erasers.webp",
+    priceLaari: 400,
+    category: "stationery",
+  },
+  {
+    id: 8901180673133,
+    name: "Faber-Castell",
+    description: "Sphere Mathematical Drawing instrument Box",
+    image: "Images/castell-geometry.jpg",
+    priceLaari: 4500,
+    category: "stationery",
   },
 ];
