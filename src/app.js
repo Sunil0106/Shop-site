91e286d8bc94d32b784220b7e06680972ee7486f
sunilbohora1010@gmail.com <sunilG>
2025-08-17 17:29:55 +0500

    Finising the initial design of the main

--- src/app.js
+++ src/app.js
@@ -3,7 +3,17 @@ import { products } from "../data/products.js";
 console.log(products);
 let html = "";
 products.forEach((product, index) => {
-  html += `<p>${product.name}</p>`;
+  html += ` <div class="content-grid">
+          <div class="image-content">
+            <img src="${product.image}" alt="${product.name}">
+          </div>
+          <div class="product-details">
+            <h4 class="product-name">${product.name}</h4>
+            <p class="product-desc">${product.description}
+              <p class="product-price">$${product.priceCents}</p>
+            </p>
+          </div>
+        </div>`;
 });
 
-document.querySelector("body").innerHTML = html;
+document.querySelector(".content-grid-container").innerHTML = html;
