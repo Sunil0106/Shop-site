88fb7f3b29e2dd13a0b8b3b6ee10ce714389d3dd
SunilG <sunilbohora1010@gmail.com>
2025-08-23 21:31:59 +0500

    Make add to Cart button interactive

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import { products } from "../data/products.js";
-import { formatCurrency, calTotalUnitPrice } from "./utils/money.js";
-import { changeTheme } from "./utils/changemode.js";
-
+import { cart } from "../data/cart.js";
+import { formatCurrency } from "./utils/money.js";
 //imports
 
 let html = "";
@@ -29,109 +28,22 @@ products.forEach((product, index) => {
 
 document.querySelector(".content-grid-container").innerHTML = html;
 
-// window.addEventListener("DOMContentLoaded", () => {
-//   changeTheme();
-//   renderCartHtml();
-// });
-// // Mode toggle
+document.querySelectorAll(".js-add-to-cart-btn").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.id;
 
-// //cart
-// export function renderCartHtml() {
-//   let cart = [
-//     {
-//       id: 8888399612064,
-//       name: "Hibis Box Tissue",
-//       description: "soft white facial tissue",
-//       image: "Images/hibis-tissue.png",
-//       priceCents: 1200,
-//       category: "households",
-//       quantity: 1,
-//     },
-//     {
-//       id: 8690216360108,
-//       name: "Fatih Dust-Free Erasers",
-//       description: "Phthalate free, Latex-free",
-//       image: "Images/fatih-erasers.webp",
-//       priceCents: 400,
-//       category: "stationery",
-//       quantity: 1,
-//     },
-//     {
-//       id: 8901180673133,
-//       name: "Faber-Castell",
-//       description: "Sphere Mathematical Drawing instrument Box",
-//       image: "Images/castell-geometry.jpg",
-//       priceCents: 4500,
-//       category: "stationery",
-//       quantity: 1,
-//     },
-//   ];
+    let matchingItem;
+    cart.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({ productId, quantity: 1 });
+    }
 
-//   document.querySelectorAll(".js-add-to-cart-btn").forEach((button) => {
-//     button.addEventListener("click", (e) => {
-//       let matchingProductId = Number(e.target.dataset.id);
-//       let matchingProduct;
-//       products.forEach((product) => {
-//         if (product.id === matchingProductId) {
-//           matchingProduct = product;
-//         }
-//       });
-//       let matchingCartItem;
-//       cart.forEach((cartItem) => {
-//         if (matchingProductId === cartItem.id) {
-//           matchingCartItem = cartItem;
-//         }
-//       });
-//       if (matchingCartItem) {
-//         matchingCartItem.quantity += 1;
-//       } else {
-//         cart.push({ ...matchingProduct, quantity: 1 });
-//       }
-//     });
-//   });
-//   let renderCart;
-//   cart.forEach((cartItem) => {
-//     renderCart += `
-// <div class="cart-product">
-//               <div class="product-image-name-content">
-//                 <div class="image-content">
-//                   <img src="${cartItem.image}" alt="${cartItem.name}" />
-//                 </div>
-//               </div>
-//               <div class="product-unit-price-content">
-//                 <h4>Unit Price</h4>
-//                 <p class="product-unit-price">MVR ${formatCurrency(
-//                   cartItem.priceCents
-//                 )}</p>
-//               </div>
-//               <div class="product-quantity-unit-content">
-//                 <input
-//                   type="number"
-//                   value="01"
-//                   placeholder="100"
-//                   class="product-cart-quantity"
-//                   min="01"
-//                 />
-//                 <select name="product-unit" class="product-cart-unit">
-//                   <option value="kg">kg</option>
-//                   <option value="grams">gms</option>
-//                 </select>
-//               </div>
-//               <div class="total-unit-price-content">
-//                 <h4>Total Price</h4>
-//                 <p class="total-unit-price">MVR${calTotalUnitPrice(
-//                   10,
-//                   cartItem.priceCents
-//                 )}</p>
-//               </div>
-//               <button  data-id="cartItem-${
-//                 cartItem.id
-//               }"class="remove-cart-item">
-//                 <i class="fa-solid fa-xmark"></i>
-//               </button>
-//             </div>
-
-//       `;
-//   });
-
-// }
+    console.log(cart);
+  });
+});
